dce4e755b965f081994a59ae0400db064ab0616d
ShufflerBardOnTheEdge <matyas.parrag@gmail.com>
2022-02-12 13:51:21 +0000

    Bugger

--- useful_funks.py
+++ useful_funks.py
@@ -16,17 +16,20 @@ class german:
     def get_gender(self,word):
         if word in self.awords[:,0]:
             ind = np.where(self.awords == word)
+            pos = []
             for i in ind[0]:
-                if self.awords[i][1] != "pl":
-                    return self.awords[i][1]
-                    
-    def dative(self,word,plural):
+                pos.append(self.awords[i][1])
+            pos = list(set(pos))
+            return pos
+
+    def dative_aw(self,word):
         theword = word.split(" ")
         the = theword[0]
         word = theword[1]
+        gen = self.get_gender(word)
         if the[0].lower() == "d" and len(the)==3:
             if the.lower()=="die":
-                if not plural:
+                if "pl" not in gen:
                     the = "der"
                 else:
                     the = "den"
@@ -36,16 +39,21 @@ class german:
                 the = "dem"
             return the + " " + word
         else:
+            if the[-2:] == "er":
+                new_the = the[:-2]+"em"
+                return new_the+" "+word
+            if the[-2:] == "es":
+                new_the = the[:-2]+"em"
+                return new_the+" "+word
             if the[-1].lower() == "e":
-                if not plural:
+                if "pl" not in gen:
                     the=the+"r"
                 else:
                     the=the+"n"
             else:
-                gen = self.get_gender(word)
-                if(gen == "n"):
+                if("n" in gen):
                     the = the+"em"
-                if(gen == "m"):
+                if("m" in gen):
                     the = the +"em"
             return the + " " + word
 
@@ -64,6 +72,8 @@ print(g.get_gender("baum"))
 print(g.get_gender("eule"))
 print(g.get_gender("wien"))
 
-print(g.dative("eine frau",False))
-print(g.dative("kein hund",False))
-print(g.dative("ein wasser",False))
+print(g.dative_aw("eine frau"))
+print(g.dative_aw("kein hund"))
+print(g.dative_aw("ein wasser"))
+print(g.dative_aw("die wasser"))
+print(g.dative_aw("jeder hund"))
